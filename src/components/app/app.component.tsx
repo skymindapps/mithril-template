c3b8b58da8ef7714bf7b29f2069bc1c874a77f23
Thomas Moller Jorgensen <thomas@blue-t.dk>
2018-05-06 22:53:53 +0200

    Routing working with observable

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -4,15 +4,18 @@ import * as t from 'i18n4v';
 import { Inject, IRouteService } from '../../services';
 import { RouteConfig, RouteConfigDefault } from '../../configs/route.config';
 import { TYPES } from '../../configs/injector.config';
+import { Subscription } from 'rxjs';
 
 interface Attrs {
 	name: string;
 }
 
 export class AppComponent implements ClassComponent<Attrs> {	
-
   @Inject(TYPES.RouteService)
   private _routeService: IRouteService;
+
+  private _routeSubscription: Subscription;
+  private _activeMenu: string;
 
 	view(vnode: Vnode<Attrs, this>): any {
 		return (
@@ -22,8 +25,8 @@ export class AppComponent implements ClassComponent<Attrs> {
         </div>
         <div class="navbar">
           <ul>
-          <li><a href="javascript:void(0)" click={() => this._navigate('/items')}>{t('app.nav.items')}</a></li>
-          <li><a href="javascript:void(0)" click={() => this._navigate('/profile')}>{t('app.nav.profile')}</a></li>
+          <li className={this._activeMenu === 'items' ? 'active' : ''}><a href="javascript:void(0)" onclick={() => this._navigate('/items')}>{t('app.nav.items')}</a></li>
+          <li className={this._activeMenu === 'profile' ? 'active' : ''}><a href="javascript:void(0)" onclick={() => this._navigate('/profile')}>{t('app.nav.profile')}</a></li>
           </ul>
         </div>
         <div class="main-stage"/>
@@ -31,11 +34,35 @@ export class AppComponent implements ClassComponent<Attrs> {
   }
 
   oncreate(vnode: Vnode<Attrs, this>) {
+    this._routeSubscription = this._routeService.currentUrl.subscribe(next => {
+      this._setActiveMenu(next);
+    });
+
     const mainStage = document.querySelector('.main-stage') as Element;
     m.route(mainStage, RouteConfigDefault, RouteConfig);
+    
+    let url = window.location.hash;
+    if (url.startsWith('#!')) {
+      url = url.substr(2);
+    }
+    this._setActiveMenu(url);
+  }
+
+  onremove(): void {
+    if (this._routeSubscription && !this._routeSubscription.closed) {
+      this._routeSubscription.unsubscribe();
+    }
   }
 
   private _navigate(url: string): void {
     this._routeService.navigate(url);
+  }
+
+  private _setActiveMenu(url: string): void {
+    if (url.toLowerCase().startsWith('/profile')) {
+      this._activeMenu = 'profile';
+    } else {
+      this._activeMenu = 'items';
+    }
   }
 }
--- src/services/route.service.ts
+++ src/services/route.service.ts
@@ -1,11 +1,20 @@
 import * as m from 'mithril';
+import { Observable, Subject } from 'rxjs';
 
 export interface IRouteService {
+  currentUrl: Subject<string>;
   navigate(url: string): void;
 }
 
-export class RouteService {
+export class RouteService implements IRouteService {
+  readonly currentUrl: Subject<string>;
+
+  constructor() {
+    this.currentUrl = new Subject<string>();
+  }
+
   navigate(url: string): void {
     m.route.set(url);
+    this.currentUrl.next(url);
   }
 }
